d48fa991f01f96eb704f27c3232a747d682adbeb
morgancfarrow90 <63519344+morgancfarrow90@users.noreply.github.com>
2020-10-27 19:47:36 -0400

    installed express

--- server.js
+++ server.js
@@ -1,29 +1,25 @@
-//server.js
-const http = require('http'),
-      url = require('url'),
+const express = require('express'),
+      server = express();
 
-makeServer = function (request,response){
-   let path = url.parse(request.url).pathname;
-   console.log(path);
-   if(path === '/'){
-      response.writeHead(200,{'Content-Type':'text/plain'});
-      response.write('Hello world');
-   }
-   else if(path === '/about'){
-     response.writeHead(200,{'Content-Type':'text/plain'});
-     response.write('About page');
-   }
-   else if(path === '/blog'){
-     response.writeHead(200,{'Content-Type':'text/plain'});
-     response.write('Blog page');
-   }
-   else{
-     response.writeHead(404,{'Content-Type':'text/plain'});
-     response.write('Error page');
-   }
-   response.end();
- },
-server = http.createServer(makeServer);
-server.listen(3000,()=>{
- console.log('Node server created at port 3000');
+server.set('port', process.env.PORT || 3000);
+
+//Basic routes
+server.get('/', (request,response)=>{
+   response.send('Home page');
+});
+
+server.get('/about',(request,response)=>{
+   response.send('About page');
+});
+
+//Express error handling middleware
+server.use((request,response)=>{
+   response.type('text/plain');
+   response.status(505);
+   response.send('Error page');
+});
+
+//Binding to a port
+server.listen(3000, ()=>{
+  console.log('Express server started at port 3000');
 });
